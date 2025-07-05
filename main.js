3c60f7bc6128de4177343d0b121f1326312885a9
Aziz <azizabdunabiyev101@gmail.com>
2025-07-05 11:01:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,315 @@
+// // Umumiy tizim va dastur boshqaruvi:
+// // Ctrl + C – Nusxa olish (Copy).
+// // Ctrl + V – Qo‘shish (Paste).
+// // Ctrl + X – Kesib olish (Cut).
+// // Ctrl + Z – Bekor qilish (Undo).
+// // Ctrl + Y – Qayta bajarish (Redo).
+// // Alt + Tab – Ilovalar o‘rtasida o‘tish.
+// // Alt + F4 – Joriy oynani yopish.
+// // Ctrl + S – Saqlash.
+// // Ctrl + P – Chop etish (Print).
+// // Ctrl + A – Hamma narsani tanlash (Select All).
+
+// // Matn muharriri uchun:
+// // Ctrl + F – Matnda izlash (Find).
+// // Ctrl + H – Matnda almashtirish (Replace).
+// // Ctrl + B – Qalin matn (Bold) (ko‘pincha ofis dasturlarida).
+// // Ctrl + I – Egik matn (Italic).
+// // Ctrl + U – Tagiga chizilgan matn (Underline).
+// // Ctrl + Shift + >/< – Matn shriftini kattalashtirish/kichraytirish.
+// // Ctrl + Shift + L – Ro‘yxat yaratish (bullet/number list).
+
+// // Fayllar va papkalar bilan ishlash:
+// // Ctrl + N – Yangi hujjat yoki oyna ochish.
+// // Ctrl + O – Fayl ochish.
+// // Shift + Delete – Faylni butunlay o‘chirish (o‘tkazib bo‘lmaydigan tarzda).
+// // Ctrl + D – Tezkor nusxa ko‘paytirish (ba'zi dasturlarda).
+
+// // nternet brauzerlar uchun:
+// // Ctrl + T – Yangi yorliq (tab) ochish.
+// // Ctrl + W – Joriy yorliqni yopish.
+// // Ctrl + Shift + T – Yopilgan yorliqni qayta ochish.
+// // Ctrl + L – Manzil satrini tanlash.
+// // Ctrl + Tab – Yorliqlarni oldinga almashtirish.
+// // Ctrl + Shift + Tab – Yorliqlarni orqaga almashtirish.
+
+// // Operatsion tizim funksiyalari:
+// // Windows + D – Ish stoli ko‘rinishini ochish/yopish.
+// // Windows + E – Fayl boshqaruvchisini ochish (Explorer).
+// // Windows + L – Kompyuterni bloklash (Lock).
+
+// // Windows 1
+
+
+
+// "K": Overclock qilish imkoniyatiga ega protsessor.
+// "F": Grafik yadrosi yo‘q (integratsiyalangan grafik mavjud emas).
+// "KF": Grafik yadrosi yo‘q, lekin overclock qilinadi.
+// "T": Tejamkorlik (past quvvat talabi).
+// "H": Noutbuklar uchun yuqori unumdorlik.
+// "U": Ultra past quvvat talabi (noutbuklar uchun).
+// "X" yoki "XE": Enthusiast darajasidagi protsessorlar (odatda Core X seriyasida).
+// Windows 2
+
+
+
+// dir - Joriy papkadagi fayl va papkalarni ko'rsatadi.
+// cd - Joriy papkani o'zgartiradi.
+// mkdir - Yangi papka yaratadi.
+// rmdir - Papkani o'chiradi (bo'sh bo'lishi kerak).
+// del - Fayllarni o'chiradi.
+// copy - Fayl yoki papkani nusxalaydi.
+// move - Fayl yoki papkani ko'chiradi.
+// ren - Fayl yoki papkani qayta nomlaydi.
+// cls - Ekranni tozalaydi.
+// exit - CMD ni yopadi.
+// echo - Xabar chiqaradi yoki o'zgaruvchini ko'rsatadi.
+// type - Matnli fayl tarkibini ko'rsatadi.
+// tree - Papkalar tarkibining tuzilishini ko'rsatadi.
+// help - Buyruqlar ro'yxati va ma'lumot beradi.
+// taskkill - Jarayonlarni to'xtatadi.
+// ping - Tarmoq ulanishini tekshiradi.
+// ipconfig - Tarmoq sozlamalarini ko'rsatadi.
+// format - Diskni formatlaydi.
+// shutdown - Kompyuterni o'chiradi yoki qayta yuklaydi.
+// sfc - Tizim fayllarini tekshiradi va tiklaydi.
+// title - CMD oynasi sarlavhasini o'zgartiradi.
+// color - CMD matn rangi va fonini o'zgartiradi.
+// time - Hozirgi vaqtni ko'rsatadi yoki o'zgartiradi.
+// date - Hozirgi sanani ko'rsatadi yoki o'zgartiradi.
+// ver - Windows versiyasini ko'rsatadi.
+// pause - Buyruqlarni vaqtincha to'xtatadi.
+// start - Ilova yoki faylni ochadi
+// netsh wlan show profiles - unitilgan wifi maulumotlarini olish
+// Windows 3
+
+
+
+// // * {
+// //     margin: 0;
+// //     padding: 0;
+// // }
+
+
+// //     .wrapper{
+// //     width: 100 %;
+// //     height: 100vh;
+// //     background - color: chocolate;
+// //     text - align: center;
+// // }
+
+// // .form{
+// //     background - color: white;
+// //     display: inline - block;
+// //     padding: 50px;
+// //     margin - top: 130px;
+// // }
+
+// // input{
+// //     width: calc(100 % - 25px);
+// //     height: 50px;
+// //     padding - left: 25px;
+// //     background - color: rgb(221, 217, 217);
+// //     border: none;
+// //     outline: none;
+// // }
+
+// // .password{
+// //     margin - top: 30px;
+// // }
+
+// // .btn{
+// //     width: 100 %;
+// //     height: 50px;
+// //     background - color: orangered;
+// //     margin - top: 30px;
+// //     border: none;
+// // }
+
+// // .text{
+// //     text - align: center;
+// //     margin - top: 30px;
+// // }
+
+
+
+// // let n = 3;
+
+// // let yigindi = 0;
+
+
+// // for (let i = 1; i <= n; i++) {
+// //     yigindi = yigindi + 1 / i;
+// // }
+
+// // console.log(yigindi);
+
+
+
+// // let n = 4;
+// // let summ = 0;
+
+// // for(let i=0 ; i<=n ; i++){
+// //     summ = Math.pow((n + i) , 2);
+// // }
+
+// // console.log(summ);
+
+
+
+// // let n = 2;
+// // let kom = 1;
+// // for(let i = 1; i<=n ; i++ ){
+// //     kom = kom * (1 + i/10);
+// // }
+
+// // console.log(kom);
+
+
+
+// // let n = 5;
+// // let ishora = 1;
+// // let sum = 0;
+
+// // for(let i = 1 ;  i<=n ; i++){
+// //     sum = sum+ishora*(1+i/10);
+// //     ishora = ishora * (-1);
+// // }
+
+// // console.log(sum);
+
+
+
+// // let n = 5;
+// // let A = 4;
+// // let B = 8;
+// // let arr = [];
+// // arr[0] = A;
+// // arr[1] = B;
+// // arr[2] = A+B;
+
+// // for(let i=3 ; i<n ; i++ ){
+// //     arr[i] = arr[i-1]*2;
+// // }
+
+// // console.log(arr);
+
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let k = 3;
+
+let newArr = [];
+for (let i = k; i < arr.length; i = i + k) {
+    newArr.push(arr[i]);
+}
+
+console.log(newArr);
+
+
+
+function oquvchiniTop(ism, group) {
+    let isHas = false;
+    for (let i = 0; i < group.length; i++) {
+        if (group[i] === ism) {
+            isHas = true;
+        }
+    }
+
+    if (isHas === true) {
+        return "Bor"
+    } else {
+        return "Yo'q"
+    }
+}
+
+console.log(oquvchiniTop("suxrob", ["aziz", "suxrob", "temur"]));
+
+
+
+function login(username, password) {
+    if (username == "admin" && password == "admin123") {
+        return "ADMIN";
+    } else if (username == "user" && password == "user123") {
+        return "USER"
+    } else {
+        return "XATO";
+    }
+}
+
+let mahsulotList = [];
+for (let a = 0; a < 100; a = a) {
+    let username = prompt("Usernameni kiriting...");
+    let password = prompt("Passwordni kiriting...");
+    let buQandayFoydalanuvchi = login(username, password);
+
+    if (buQandayFoydalanuvchi == "ADMIN") {
+        for (let i = 0; i < 100; i = i) {
+            let menu = prompt("1. Mahsulot qo'shish \n 2. Chiqish");
+
+            if (menu == 1) {
+                let mahsulot = prompt("Mahsulot nomini kiriting...");
+                mahsulotList.push(mahsulot);
+            } else if (menu == 2) {
+                i = 101;
+            }
+        }
+
+
+    } else if (buQandayFoydalanuvchi == "USER") {
+        for (let j = 0; j < 100; j = j) {
+            let menu1 = prompt("1. Mahsulotlarni ko'rish \n 2.Chiqish");
+
+            if (menu1 == 1) {
+                alert(mahsulotList);
+            } else if (menu1 == 2) {
+                j = 101;
+            }
+        }
+    } else {
+        alert("Login parol xato")
+    }
+
+}
+
+
+
+let users = [];
+
+for (let j = 0; j < 1; j = j) {
+
+    let mainMenu = prompt("1.Login \n 2.Register \n 3.Exit");
+
+    if (mainMenu == 1) {
+        let username = prompt("Username");
+        let password = prompt("Password");
+
+        for (let i = 0; i < users.length; i++) {
+            if (username == users[i].username && password == users[i].password) {
+                alert("Xush kelibzis 🙂 " + users[i].fio);
+            }
+        }
+
+
+    } else if (mainMenu == 2) {
+        let fio = prompt("F.I.O");
+        let age = prompt("Yosh")
+        let phoneNumber = prompt("Telefon raqam")
+        let username = prompt("Username")
+        let password = prompt("Parol")
+
+        let user = {
+            fio: fio,
+            age: age,
+            phoneNumber: phoneNumber,
+            username: username,
+            password, password
+        }
+
+        users.push(user);
+    } else {
+        j = 1;
+    }
+
+}
